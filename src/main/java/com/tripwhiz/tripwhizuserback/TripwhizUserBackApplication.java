95bf8b5af1caae608dadb9be610bbcad1b19c707
진성오 <wlsqoren@naver.com>
2024-11-05 16:42:22 +0900

    contentUpdate_SO

--- src/main/java/com/tripwhiz/tripwhizuserback/TripwhizUserBackApplication.java
+++ src/main/java/com/tripwhiz/tripwhizuserback/TripwhizUserBackApplication.java
@@ -1,13 +1,18 @@
 package com.tripwhiz.tripwhizuserback;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class TripwhizUserBackApplication {
 
     public static void main(String[] args) {
+
+        log.info("--------------------------");
         SpringApplication.run(TripwhizUserBackApplication.class, args);
+        log.info("--------------------------");
     }
 
 }
