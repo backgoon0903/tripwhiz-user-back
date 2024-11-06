66f3b7a24270d4f7e334a2c397655e4fbd38e84a
eggzuxi <eggzuxi99@gmail.com>
2024-11-06 17:47:54 +0900

    productEntityAdd_JH

---
+++ src/main/java/com/tripwhiz/tripwhizuserback/product/domain/Product.java
@@ -0,0 +1,49 @@
+package com.tripwhiz.tripwhizuserback.product.domain;
+
+//import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"attachFiles"})
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    @Column(nullable = false, length = 50)
+    private String pname;
+
+    @Lob
+    private String pdesc;
+
+    private int price;
+
+    @ElementCollection
+    @Builder.Default
+    private Set<AttachFile> attachFiles = new HashSet<>();
+
+    private boolean delFlag;
+
+    public void changeDelFlag(boolean newDelFlag) {
+        this.delFlag = newDelFlag;
+    }
+
+    public void addFile(String filename){
+        attachFiles.add(new AttachFile(attachFiles.size(), filename));
+    }
+
+    public void clearFiles(){
+        attachFiles.clear();
+    }
+
+}
