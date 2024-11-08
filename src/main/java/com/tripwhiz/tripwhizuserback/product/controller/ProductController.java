3d5dbac3d0d5aaad6988d094c3c7a4839d36e1ee
USER <ckalswn371004@gmail.com>
2024-11-08 20:56:57 +0900

    productlist완료-MJ

--- src/main/java/com/tripwhiz/tripwhizuserback/product/controller/ProductController.java
+++ src/main/java/com/tripwhiz/tripwhizuserback/product/controller/ProductController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/product")
 @Log4j2
 @RequiredArgsConstructor
 public class ProductController {
