c5f428936b4f411a2bbd281ef167a66361d7df25
eggzuxi <eggzuxi99@gmail.com>
2024-11-13 16:55:49 +0900

    cartUpdate_JH

--- src/main/java/com/tripwhiz/tripwhizuserback/cart/domain/Cart.java
+++ src/main/java/com/tripwhiz/tripwhizuserback/cart/domain/Cart.java
@@ -1,7 +1,26 @@
 package com.tripwhiz.tripwhizuserback.cart.domain;
 
+import com.tripwhiz.tripwhizuserback.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"product"})
+@Table(name = "cart")
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @Column(nullable = false)
+    private int qty;
 
 }
--- src/test/java/com/tripwhiz/tripwhizuserback/cart/repository/CartRepoTests.java
+++ src/test/java/com/tripwhiz/tripwhizuserback/cart/repository/CartRepoTests.java
@@ -1,10 +1,16 @@
 package com.tripwhiz.tripwhizuserback.cart.repository;
 
+import com.tripwhiz.tripwhizuserback.cart.domain.Cart;
+import com.tripwhiz.tripwhizuserback.product.domain.Product;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+
+import java.util.stream.IntStream;
 
 @DataJpaTest
 @Log4j2
@@ -17,8 +23,42 @@ public class CartRepoTests {
     @Test
     public void cartTest() {
 
-        log.info(cartRepository.findAll().toString());
+        log.info(cartRepository.findAll());
 
     }
+
+    @Test
+    @Transactional
+    @Commit
+    public void insertDummies() {
+
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+
+            Cart cart = Cart.builder()
+                    .product(Product.builder().pno((long) (Math.random() * 10 + 1)).build()) // product_pno는 무작위 값
+                    .qty((int) (Math.random() * 5) + 1) // qty는 1에서 5 사이의 무작위 값
+                    .build();
+
+            cartRepository.save(cart);
+
+        });
+
+    }
+
+//    @Test
+//    @Transactional
+//    @Commit
+//    public void insertDummies() {
+//        MemberEntity member = MemberEntity.builder().mno(1L).build(); // member는 이미 존재하는 1번 회원을 사용
+//        IntStream.rangeClosed(1, 10).forEach(i -> {
+//            Product product = Product.builder().pno((long) (Math.random() * 10 + 1)).build(); // 무작위 product_pno 사용
+//            Cart cart = Cart.builder()
+//                    .member(member) // member를 1로 고정
+//                    .product(product) // product 무작위 값
+//                    .qty((int) (Math.random() * 5) + 1) // qty는 1에서 5 사이의 무작위 값
+//                    .build();
+//            cartRepository.save(cart);
+//        });
+//    }
 
 }
